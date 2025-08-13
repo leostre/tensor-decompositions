7939a11c0f3f221ed5719ed6621e35b7c073cef2
Irina Usoltseva <irinaslcv@gmail.com>
2025-08-13 16:41:45 +0300

    Updated sampling methods

--- tdecomp/matrix/sampling_techniques.py
+++ tdecomp/matrix/sampling_techniques.py
@@ -1,34 +1,38 @@
 import torch
 
-def adaptive_sampling(A: torch.Tensor, k: int, s: int, n_iter: int = 3) -> torch.Tensor:
+__all__ = [
+    'adaptive_sampling',
+    'column_select'
+]
+
+def adaptive_sampling_inv(X: torch.Tensor, k: int, s: int, n_iter: int = 3) -> tuple[torch.Tensor, torch.Tensor]:
     """
     Улучшенная версия adaptive_sampling (https://arxiv.org/pdf/0708.3696) с явным решением LS задач
     Args:
-        A: входная матрица (m x n)
+        X: входная матрица (m x n)
         k: число столбцов для первой выборки
         s: число дополнительных столбцов
         n_iter: количество итераций уточнения
     Returns:
-        C: матрица выбранных столбцов (m x (k+s))
+        S, B
     """
-    initial_cols = torch.randperm(A.size(1))[:k]
-    C = A[:, initial_cols]
+    X_pinv = torch.linalg.pinv(X)  
+    initial_cols = torch.randperm(X_pinv.size(1))[:k]
+    S = X_pinv[:, initial_cols]
     
     for _ in range(n_iter):
-        X = torch.linalg.lstsq(C, A).solution
-        
-        residual = A - C @ X
-        
+        B = torch.linalg.lstsq(S, X_pinv).solution
+        residual = X_pinv - S @ B
         probs = torch.norm(residual, p=2, dim=0)**2
         probs /= probs.sum()
-        
         extra_cols = torch.multinomial(probs, s, replacement=False)
-        C = torch.cat([C, A[:, extra_cols]], dim=1)
+        S = torch.cat([S, X_pinv[:, extra_cols]], dim=1)
     
-    return C
+    B = torch.linalg.lstsq(S, X_pinv).solution
+    return S, B
 
 
-def column_select_ls(A: torch.Tensor, k: int, epsilon: float = 0.1) -> torch.Tensor:
+def column_select_inv(X: torch.Tensor, k: int, epsilon: float = 0.1) -> tuple[torch.Tensor, torch.Tensor]:
     """
     Модифицированный ColumnSelect (file:///C:/Users/user/Downloads/Telegram%20Desktop/Randomized_Algorithms_for_Computation_of_Tucker_Decomposition_and.pdf)
     Args:
@@ -36,22 +40,22 @@ def column_select_ls(A: torch.Tensor, k: int, epsilon: float = 0.1) -> torch.Ten
         k: Целевой ранг аппроксимации
         epsilon: Параметр точности
     Returns:
-        C: Матрица выбранных столбцов (m x c)
+        S, B
     """
-    m, n = A.shape
-    
-    Q, R = torch.linalg.qr(A)
+    X_pinv = torch.linalg.pinv(X)
+    m, n = X_pinv.shape
+    Q, _ = torch.linalg.qr(X_pinv)
     U = Q[:, :k]  
-    
     leverage_scores = torch.norm(U, dim=1)**2
     c = int(k * torch.log(torch.tensor(k)) / epsilon**2)
     c = min(c, n)
-    
     probs = leverage_scores / k
     selected_cols = torch.multinomial(probs, c, replacement=True)
-    
-    C = A[:, selected_cols]
-    scales = torch.linalg.lstsq(C, A).solution.norm(dim=1)
-    C = C * scales.reshape(1, -1)
-    
-    return C
+    S = X_pinv[:, selected_cols]
+    B = torch.linalg.lstsq(S, X_pinv).solution
+    return S, B
+
+SAMPLING_TECHNIQUES = {
+        'adaptive_sampling' : adaptive_sampling_inv,
+        'column_select' : column_select_inv
+    }
