024ae8c39d2ac5fd75f08bcff6d86c715c28d4d9
leostre <strelllleo@mail.ru>
2026-01-21 18:02:06 +0300

    enhanced tests

--- tests/matrix/test_matrix.py
+++ tests/matrix/test_matrix.py
@@ -7,15 +7,18 @@ from tdecomp._base import Decomposer
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-X = torch.randn(100, 100, device=DEVICE)
 RTOL = 1e-4
+
+Xs = [
+    torch.randn(100, 100, device=DEVICE),
+    torch.randn(200, 100, device=DEVICE),
+    torch.randn(100, 200, device=DEVICE),
+]
 
 @pytest.mark.parametrize('name', DECOMPOSERS.keys())
 def test_decomposer_relative_error(name):
-    decomposer: Decomposer = DECOMPOSERS[name](rank=min(X.size()))
-    dec_result = decomposer.decompose(X, 100)
-    error = decomposer.get_approximation_error(X, *dec_result, relative=True)
-    assert error < RTOL, f'{name} returns approximation violating rtol: error = {error} & relative tolerance = {RTOL}'
-
-
-    
+    for X in Xs:
+        decomposer: Decomposer = DECOMPOSERS[name](rank=min(X.size()))
+        dec_result = decomposer.decompose(X, 100)
+        error = decomposer.get_approximation_error(X, *dec_result, relative=True)
+        assert error < RTOL, f'{name} returns approximation violating rtol: error = {error} & relative tolerance = {RTOL}. Size: {X.size()}'
